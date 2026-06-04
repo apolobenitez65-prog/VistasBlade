1e20dfca99a24466a3827b398c974a6efbb663bd
apolo <apolobenitez65@gmail.com>
2026-06-04 00:01:51 -0300

    cambie el nombre del sitio

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,7 +62,7 @@
 
 <body>
     <header>
-        <h1>Mi sitio Laravel</h1>
+        <h1>Bienvenido a mi Negocio Online</h1>
         <nav>
             <a href="/">Inicio</a>
             <a href="/productos">Productos</a>
