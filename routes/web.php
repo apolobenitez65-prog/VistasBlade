4678d0179e61812121d23c542feb8124432c7ba9
apolo <apolobenitez65@gmail.com>
2026-06-04 00:08:14 -0300

    Cree una ruta /nosotros.

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('inicio');
 });
+Route::get('/nosotros', function () {
+    return view('nosotros');
+});
 Route::get('/productos', function () {
     $productos = [
          [
