afdaeca47c2b933910e051f2038248e67aa0b5b4
apolo <apolobenitez65@gmail.com>
2026-06-04 00:11:36 -0300

    Agrege un enlace de Nosotros en el menú.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -66,6 +66,7 @@
         <nav>
             <a href="/">Inicio</a>
             <a href="/productos">Productos</a>
+            <a href="/nosotros">Nosotros</a>
             <a href="/contacto">Contacto</a>
         </nav>
     </header>
